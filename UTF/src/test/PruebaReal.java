27ecb1671021aa98f372e68cbfa7e560ae9dff09
Mariova15 <43605653+Mariova15@users.noreply.github.com>
2019-03-28 19:00:54 +0100

    EscribirClavesRegistro
    Fallo al borrar claves de registro

--- UTF/src/test/PruebaReal.java
+++ UTF/src/test/PruebaReal.java
@@ -7,9 +7,15 @@ package test;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.nio.file.Files;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import logica.OperacionesFicheros;
 import utils.DescargaRecursos;
+import utils.WinRegistry;
 
 /**
  *
@@ -22,31 +28,46 @@ public class PruebaReal {
      */
     public static void main(String[] args) throws IOException {
         //String archivodescargar = "http://fonts.gstatic.com/s/lato/v14/S6uyw4BMUTPHvxk.ttf";
-        
+
         //DescargaRecursos.descargarArchivo(archivodescargar, "lato.ttf", "");
-        
         //File dir = new File("C:\\Windows\\Fonts\\lato.ttf");
-        
         //Copia fichero en directorio Fonts
-        File dir = new File("C:\\Windows\\Fonts\\lato.ttf");        
-        File font = new File("lato.ttf");        
+        File dir = new File("C:\\Windows\\Fonts\\lato.ttf");
+        File font = new File("lato.ttf");
         //Files.copy(font.toPath(), dir.toPath());
-        
+
         //Escribe clave de registro
-        
-        //https://stackoverflow.com/questions/62289/read-write-to-windows-registry-using-java
-        
-        //dir.delete();
-        
-       /* System.out.println(dir.delete());
-       
-        OperacionesFicheros algo = new OperacionesFicheros();                        
-        for (File fuente : algo.listarArchivos(algo.listarDirectoriosNoVacios("C:\\Windows\\Fonts"))) {
+        /*
+        \[HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts\] "Lato (TrueType)"="lato.ttf"
+         */
+        String name = "Lato (TrueType)";
+
+        String valor = "lato.ttf";
+
+        String key = "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Fonts";
+
+        String llaveRegistro = "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Fonts";
+
+        try {
+
+            //WinRegistry.writeStringValue(WinRegistry.HKEY_LOCAL_MACHINE, key, name, valor);
+            WinRegistry.deleteKey(WinRegistry.HKEY_LOCAL_MACHINE, key + "\\" + valor);
+
+            //https://stackoverflow.com/questions/62289/read-write-to-windows-registry-using-java
+            //dir.delete();
+            /* System.out.println(dir.delete());
+            OperacionesFicheros algo = new OperacionesFicheros();
+            for (File fuente : algo.listarArchivos(algo.listarDirectoriosNoVacios("C:\\Windows\\Fonts"))) {
             System.out.println(fuente.getName());
-        }*/
-       
-        
-        
+            }*/
+        } catch (IllegalArgumentException ex) {
+            Logger.getLogger(PruebaReal.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(PruebaReal.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InvocationTargetException ex) {
+            Logger.getLogger(PruebaReal.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
-    
+
 }
